828c979a9164796733aced123e1f97ebd8a273d2
Twelveeee <Twelveeee12@gmail.com>
2019-08-29 03:46:07 +0800

    L1

---
+++ L1/L1-060/main.py
@@ -0,0 +1,2 @@
+x,y=map(int,input().split())
+print('%d'%((x-y)*50))
---
+++ L1/L1-061/main.py
@@ -0,0 +1,7 @@
+x,y=map(float,input().split())
+c=float(x/(y**2))
+print('%.1f'%c)
+if c>25 :
+    print('PANG')
+else:
+    print('Hai Xing')
