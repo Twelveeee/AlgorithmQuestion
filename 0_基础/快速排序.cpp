48a4474b0deffc74d462705ffcd8300654e08284
Twelveeee <Twelveeee12@gmail.com>
2021-04-16 11:29:49 +0800

    Update 快速排序.cpp

--- 0_基础/快速排序.cpp
+++ 0_基础/快速排序.cpp
@@ -1,37 +1,33 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
-#include <map>
-
 using namespace std;
 
-
-int QKparr(int arr[],int low,int high){
-    int povot = arr[low];
+int kps(vector<int> &arr,int low,int high){
+    int anchor = arr[low];
     while (low<high){
-        /* high 从右到左找小于 x.key 的记录 */
-        while (low<high && arr[high] >= povot) high--;
-        arr[low]=arr[high];
-        while (low<high && arr[low] <= povot) low++;
-        arr[high]=arr[low];
+        while (low<high && arr[high]>=anchor) high--;
+        arr[low] = arr[high];
+        while (low<high && arr[low]<=anchor) low++;
+        arr[high] = arr[low];
     }
-    arr[low]=povot;
+    arr[low] = anchor;
     return low;
 }
 
-void qsort(int arr[],int low,int high){
-    if(low<high){
-        int povot = QKparr(arr,low,high);
-        qsort(arr,low,povot-1);
-        qsort(arr,povot+1,high);
+void kp(vector<int> &arr,int low,int high){
+    if (low<high){
+        int anchor = kps(arr,low,high);
+        kp(arr,low,anchor-1);
+        kp(arr,anchor+1,high);
     }
 }
 
-
 int main() {
-    int n=5;
-    int arr[]={3,2,4,1,2};
-    qsort(arr,0,n-1);
-    for (int i = 0; i < n; ++i) {
-        cout << arr[i]<<endl;
+    vector<int> arr{1,8,6,3,9,2};
+
+    kp(arr,0,arr.size()-1);
+    for (int i : arr) {
+        cout << i<<endl;
     }
 }
